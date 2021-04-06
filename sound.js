fb179cb316b0b45b4e2ee5ac18aef85d7ec8e5d0
bigmuzzy <kenny@pinkclicks.com>
2021-04-06 18:09:19 +0700

    Make addSound method async

--- sound.js
+++ sound.js
@@ -4,11 +4,17 @@ class Sound {
 		this.volume = config.volume || 0.05
 	}
 	addSound(sound, file) {
-		try {
-			this.sounds[sound] = new Audio(file)
-		} catch(e) {
-			console.error(e)
-		}
+		return new Promise((resolve, reject) => {
+			try {
+				const audio = new Audio(file)
+				audio.load()
+				audio.addEventListener("canplaythrough", resolve, false)
+				this.sounds[sound] = audio
+			} catch(e) {
+				console.error(e)
+				reject(e)
+			}
+		})
 	}
 	play(name) {
 		const sound = this.sounds[name]
